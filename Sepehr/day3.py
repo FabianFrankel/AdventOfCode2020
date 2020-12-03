336bcf543303bacf4a19e8a10f109422011d5c62
Sepehr Tayari <sepehr@Sepehrs-MacBook-Pro.local>
2020-12-03 11:57:39 +0100

    Day 3 part 2 done

--- Sepehr/day3.py
+++ Sepehr/day3.py
@@ -14,19 +14,29 @@ class Day3:
         map = input.split('\n')[:-1]
         return map
 
-    def stepper(self):
+    def stepper(self, step_right, step_down):
         '''Steps through the map staring at ind 0,0 and walking three steps right one down'''
         x_len = len(self.map[0])# 31 
         y_len = len(self.map)   # 323
         tree_crash = 0
-        step_right = 3
-        #step_down = 1
-        for i in range(y_len):
-            x_index = ((i * step_right)) % x_len
-            if self.map[i][x_index] == '#':
+        for y in range(0, y_len, step_down):
+            x_index = (( int(y/step_down) * step_right)) % x_len
+            #print('x {}, y {}, sig {}'.format(x_index, y, self.map[y][x_index]))
+            #print(self.map[2][1])
+            if self.map[y][x_index] == '#':
+                #print('X {}, Y {}, non mod {}'.format(x_index, y, (( int(y/step_down) * step_right))))
                 tree_crash += 1
         return tree_crash
 
+    def multiple_slopes(self):
+        return  (   self.stepper(1,1),
+                    self.stepper(3,1),
+                    self.stepper(5,1),
+                    self.stepper(7,1),
+                    self.stepper(1,2))
+
 if __name__ == '__main__':
     puzzle3 = Day3()
-    puzzle3.stepper()
+    #print(puzzle3.stepper(7,1))
+    res = puzzle3.multiple_slopes()
+    print(res)
