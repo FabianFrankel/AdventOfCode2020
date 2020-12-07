55efc136f788f0667333dd20603772fbe4844163
Fabian Frankel <fabianfrankel@MacBook-Air-som-tillhor-Fabian.local>
2020-12-07 12:01:56 +0100

    part 2 puzzle6

--- Fabian/6/puzzle6.py
+++ Fabian/6/puzzle6.py
@@ -24,6 +24,35 @@ class customs_checker:
             ans_sum = self.count_number_of_yes_for_group(i) + ans_sum
         return ans_sum
 
+    def get_sum_of_exclusive_yes(self):
+        ans_sum = 0
+        for i in self.answers_array:
+            ans_sum = self.count_number_of_exclusive_yes_for_group(i) + ans_sum
+        return ans_sum
+
+
+    def count_number_of_exclusive_yes_for_group(self, group_answer_array):
+        print(group_answer_array)
+        answer_set = self.get_exclusive_yes_answer_set(group_answer_array)
+        return(len(answer_set))
+
+    def get_exclusive_yes_answer_set(self, group_answer_array):
+        answer_arr = []
+        next_answers = []
+        first = True
+        for i in group_answer_array:
+            for j in i:
+                if first:
+                    answer_arr.append(j)
+                elif j in answer_arr:
+                    next_answers.append(j)
+            if not first:
+                answer_arr = next_answers
+                next_answers = []    
+            first = False
+        return answer_arr
+
+
 
 
 
@@ -61,7 +90,7 @@ def get_input_array():
 if __name__ == '__main__':
     input_array = get_input_array()
     customs_checker = customs_checker(input_array)
-    print(customs_checker.get_sum_of_group_yes())
+    print(customs_checker.get_sum_of_exclusive_yes())
     
     
 
